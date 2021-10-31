f532165bf76c2835a08e7cd4d37abc6e0108390f
deer42 <d.rohlmann@outlook.de>
2021-10-31 19:00:54 +0100

    Add benchmarks

---
+++ benchmarks/QrGenerator.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace QrGenerator.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<QrGeneratorBenchmarker>();
+        }
+    }
+}
